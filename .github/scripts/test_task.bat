ac081b914e4ea027cac797aa1e854d6fc0a1bbb2
Christoph M. Becker <cmbecker69@gmx.de>
2022-06-30 14:00:18 +0200

    SKIP_IO_CAPTURE_TESTS

--- .github/scripts/test_task.bat
+++ .github/scripts/test_task.bat
@@ -1,5 +1,6 @@
 mkdir C:\artifacts
 
 set TEST_PHP_JUNIT=C:\artifacts\junit.out.xml
+set SKIP_IO_CAPTURE_TESTS=1
 
 nmake test TESTS="-j3 -q --offline --show-diff --show-slow 1000 --set-timeout 120"
