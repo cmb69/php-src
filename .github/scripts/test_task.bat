ccd47ac17e7f2b513d847cf929b0726f452058d4
Christoph M. Becker <cmbecker69@gmx.de>
2022-06-30 14:29:37 +0200

    REPORT_EXIT_STATUS

--- .github/scripts/test_task.bat
+++ .github/scripts/test_task.bat
@@ -2,5 +2,6 @@ mkdir C:\artifacts
 
 set TEST_PHP_JUNIT=C:\artifacts\junit.out.xml
 set SKIP_IO_CAPTURE_TESTS=1
+set REPORT_EXIT_STATUS=1
 
 nmake test TESTS="-j3 -q --offline --show-diff --show-slow 1000 --set-timeout 120"
