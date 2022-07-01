bd9b7c382a3d57696de602817138f9dbe83a1d44
Christoph M. Becker <cmbecker69@gmx.de>
2022-07-01 11:42:00 +0200

    extension_dir

--- .github/scripts/test_task.bat
+++ .github/scripts/test_task.bat
@@ -1,5 +1,12 @@
 mkdir C:\artifacts
 
+if exist x64\Release set PHP_BUILD_DIR=x64\Release
+if exist x64\Release_TS set PHP_BUILD_DIR=x64\Release_TS
+if exist Release set PHP_BUILD_DIR=Release
+if exist Release_TS set PHP_BUILD_DIR=Release_TS
+
+echo extension_dir=%PHP_BUILD_DIR%> %PHP_BUILD_DIR%\php.ini
+
 set TEST_PHP_JUNIT=C:\artifacts\junit.out.xml
 set SKIP_IO_CAPTURE_TESTS=1
 set REPORT_EXIT_STATUS=1
